c34368eca7bac8672e601c534ce3d5a463844162
Ryan Novotny <ryan.novotny@gmail.com>
2016-05-06 15:43:20 -0700

    make it run first(?)

--- wp-php-log.php
+++ wp-php-log.php
@@ -11,6 +11,21 @@ License: GPLv2
 Version: 1.0.0
 */
 
+function rn_wpphp_this_plugin_first() {
+	// ensure path to this file is via main wp plugin path
+	$wp_path_to_this_file = preg_replace('/(.*)plugins\/(.*)$/', WP_PLUGIN_DIR."/$2", __FILE__);
+	$this_plugin = plugin_basename(trim($wp_path_to_this_file));
+	$active_plugins = get_option('active_plugins');
+	$this_plugin_key = array_search($this_plugin, $active_plugins);
+	if ($this_plugin_key) { // if it's 0 it's the first plugin already, no need to continue
+		array_splice($active_plugins, $this_plugin_key, 1);
+		array_unshift($active_plugins, $this_plugin);
+		update_option('active_plugins', $active_plugins);
+	}
+	wp_php_log( get_option('active_plugins'), 'active-plugins' );
+}
+add_action("activated_plugin", "rn_wpphp_this_plugin_first");
+
 // BASIC SECURITY
 defined( 'ABSPATH' ) or die( 'Unauthorized Access!' );
 
